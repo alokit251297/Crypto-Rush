e54d942d077033dc72075b079b3c718ae805512d
alokit25 <alokitgupta98@gmail.com>
2022-04-18 19:23:33 -0400

    Exchanges complete backend code

--- data/exchangeApi.js
+++ data/exchangeApi.js
@@ -23,6 +23,56 @@ async function getExchange() {
   }
 };
 
+async function getExchangeList(){
+  // Make calls
+  let pData = await CoinGeckoClient.ping();
+  //console.log(pData)
+  let lData = await CoinGeckoClient.exchanges.list();
+  //console.log(lData)
+  if(lData.code===200){
+    return lData;
+  }else{
+    throw {
+      response: { status: 404, statusText: `No data found.` },
+    }
+  }
+}
+
+async function getExchangeById(id){
+  validations.validateString(id, "id");
+  // Make calls
+  let pData = await CoinGeckoClient.ping();
+  //console.log(pData)
+  let idData = await CoinGeckoClient.exchanges.fetch(id);
+  //console.log(idData)
+  if(idData.code===200){
+    return idData;
+  }else{
+    throw {
+      response: { status: 404, statusText: `No data found.` },
+    }
+  }
+}
+
+async function getTickers(id){
+  validations.validateString(id, "id");
+  // Make calls
+  let pData = await CoinGeckoClient.ping();
+  //console.log(pData)
+  let tickerData = await CoinGeckoClient.exchanges.fetchTickers(id);
+  //console.log(idData)
+  if(tickerData.code===200){
+    return tickerData;
+  }else{
+    throw {
+      response: { status: 404, statusText: `No data found.` },
+    }
+  }
+}
+
 module.exports = {
-    getExchange
+    getExchange,
+    getExchangeList,
+    getExchangeById,
+    getTickers
   };
